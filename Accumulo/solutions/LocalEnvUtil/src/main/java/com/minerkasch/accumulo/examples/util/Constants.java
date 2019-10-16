60ef2f2359183342a38d775c0cf4b3f585b8c2ef
Zachary Radtka <zachary.radtka@gmail.com>
2019-10-15 23:07:09 -0400

    Updated Solutions to accumulo 1.9.2

--- Accumulo/solutions/LocalEnvUtil/src/main/java/com/minerkasch/accumulo/examples/util/Constants.java
+++ Accumulo/solutions/LocalEnvUtil/src/main/java/com/minerkasch/accumulo/examples/util/Constants.java
@@ -3,9 +3,9 @@ package com.minerkasch.accumulo.examples.util;
 public class Constants {
 	// Accumulo Configuration
 	public static final String USER_NAME = "root";
-	public static final String USER_PASS = "password";
-	public static final String INSTANCE = "hdp-accumulo-instance";
-	public static final String ZOOKEEPERS = "localhost:2181";
+	public static final String USER_PASS = "secret";
+	public static final String INSTANCE = "instance";
+	public static final String ZOOKEEPERS = "ec2-18-191-210-82.us-east-2.compute.amazonaws.com:2181";
 
 	// Table names
 	public static final String TWITTER_TABLE = "TwitterData";
